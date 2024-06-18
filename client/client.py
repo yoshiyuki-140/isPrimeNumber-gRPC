73c32df71f2d81fa9c9588cf7275383e11d6a606
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-06-18 17:25:24 +0900

    clean : コメントを整えた

--- client/client.py
+++ client/client.py
@@ -7,14 +7,25 @@ os.environ["GRPC_VERBOSITY"] = "NONE"  # ログを非表示にする
 os.environ["NO_PROXY"] = "localhost"
 
 
-def run():
+def main():
+    """メイン関数"""
     # サーバーのアドレスとポートを指定
     with grpc.insecure_channel("localhost:9000") as channel:
         stub = isPrime_pb2_grpc.IsPrimeFuncStub(channel)
         # サーバーに送るメッセージを定義
-        response = stub.CheckPrime(isPrime_pb2.Value(Value=17))
+        response = stub.CheckPrime(isPrime_pb2.Value(Value=askNumber()))
         print("Is 17 a prime number? " + ("Yes" if response.IsPrime else "No"))
 
 
+def askNumber():
+    """数字を聞いて返すだけの関数
+
+    Returns:
+        int: 素数かどうか調べたい数字
+    """
+    num = input("Please input a number: ")
+    return int(num)
+
+
 if __name__ == "__main__":
-    run()
+    main()
