4bef02a2e9fbbc345e01d99d5908397004169758
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-06-20 15:36:40 +0900

    add : I completed python version gRPC server

--- server-py/server-py.py
+++ server-py/server-py.py
@@ -7,6 +7,8 @@ import isPrime.isPrime_pb2_grpc as isPrime_pb2_grpc
 class IsPrimeFuncServicer(isPrime_pb2_grpc.IsPrimeFuncServicer):
     def CheckPrime(self, request, context):
         number = request.Value
+        print(number)
+        # 素数判定アルゴリズム
         if number > 1:
             for i in range(2, int(number**0.5) + 1):
                 if (number % i) == 0:
@@ -16,7 +18,9 @@ class IsPrimeFuncServicer(isPrime_pb2_grpc.IsPrimeFuncServicer):
 
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10)  # 最大10スレッドで動作
+    )
     isPrime_pb2_grpc.add_IsPrimeFuncServicer_to_server(IsPrimeFuncServicer(), server)
     server.add_insecure_port("[::]:9000")  # 暗号化してない
     server.start()
